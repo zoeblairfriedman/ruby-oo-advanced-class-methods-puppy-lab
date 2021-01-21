3a1ecbcea09ddbac9c8a74ee353596017e2c3651
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-21 17:04:01 -0500

    commit tree f83cc1d8905b21712a16bc61ee0d78e055cc04d3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+require 'pry'
+
+class Dog
+attr_accessor :name
+
+@@all = []
+
+def initialize(name)
+self.name = name
+self.save
+end
+
+def save
+    @@all << self
+end
+
+def self.clear_all
+    @@all.clear
+end
+
+def self.all
+    @@all
+end
+
+def self.print_all
+    puts @@all.map {|dog| dog.name}
+end
+
+end
